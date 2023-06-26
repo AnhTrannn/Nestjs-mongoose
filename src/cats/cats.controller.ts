f487bf679e613b2d05cd9978ef9af718af1be233
Ace <qanhltv11@gmail.com>
2023-06-26 23:27:58 +0700

    Create cats module and connect mongoose

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,29 @@
+import { CatsService } from './cats.service';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Cat } from './interfaces/cats.interfaces';
+import { CreateCatDto } from './dto/create-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+    constructor(private readonly catsService: CatsService) { }
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto);
+    }
+    
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string): Promise<Cat> {
+        return this.catsService.findOne(id);
+    }
+
+    @Delete()
+    async delete(@Param('id') id: string) {
+        return this.catsService.delete(id);
+    }
+}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,28 @@
+import { CreateCatDto } from './dto/create-cat.dto';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './schemas/cat.schemas';
+
+@Injectable()
+export class CatsService {
+    constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+    async create(createCatDto: CreateCatDto): Promise<Cat> {
+        const createCat = await this.catModel.create(createCatDto);
+        return createCat;
+    }
+
+    async findAll(): Promise<Cat[]> {
+        return this.catModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<Cat> {
+        return this.catModel.findOne({_id: id}).exec();
+    }
+
+    async delete(id: string){
+        const deleteCat = await this.catModel.findOneAndRemove({_id: id}).exec();
+        return deleteCat;
+    }
+}
